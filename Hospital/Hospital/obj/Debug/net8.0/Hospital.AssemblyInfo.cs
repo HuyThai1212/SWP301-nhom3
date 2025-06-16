b6e0b59343698394cfaaf66dd943c4d93ce3609c
HuyThai1212 <hxxcantsee@gmail.com>
2025-06-17 04:17:10 +0700

    update file rieng

--- Hospital/Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
+++ Hospital/Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eafdf2502cf846cb055891f8a69acdb3ddfe1717")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
